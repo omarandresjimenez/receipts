4f8b593546043e769027b7a6c1174161e74c3ec5
OmarJimenez <omar.jimenez@innovateod.com>
2020-11-20 15:05:33 -0800

    add missing  error hadling to validatemai and recoverpwd apis

--- src/app/pages/recover-password/api/recover-password.api.ts
+++ src/app/pages/recover-password/api/recover-password.api.ts
@@ -3,15 +3,22 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 import { Observable } from 'rxjs';
+import { AppHttpErrorHandler } from 'src/app/core/utils/errorHandler';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { catchError } from 'rxjs/operators';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class RecoverPasswordApiService  {
+export class RecoverPasswordApiService extends AppHttpErrorHandler  {
   private readonly BASEURL = environment.baseUrlApi;
-  constructor(private http: HttpClient) {
-   }
+  constructor(private http: HttpClient,
+              public router: Router,
+              public toast: ToastrService) {
+     super(router, toast);
+  }
 
   public ChangePaswordMail(email: string, password: string, hash: string): Observable<boolean> {
     const params = {
@@ -19,14 +26,18 @@ export class RecoverPasswordApiService  {
         password,
         hash,
       };
-    return this.http.put<boolean>(this.BASEURL + 'user/UpdatePassword', params);
+    return this.http.put<boolean>(this.BASEURL + 'user/UpdatePassword', params).pipe(
+      catchError((err) => this.handleError(err))
+    );
   }
 
   public recoverPassword(email: string): Observable<boolean> {
     const params = {
         email
       };
-    return this.http.post<boolean>(this.BASEURL + 'user/recoverPassword', params);
+    return this.http.post<boolean>(this.BASEURL + 'user/recoverPassword', params).pipe(
+      catchError((err) => this.handleError(err))
+    );
   }
 
 }
--- src/app/pages/valida-mail/api/validamail.api.ts
+++ src/app/pages/valida-mail/api/validamail.api.ts
@@ -3,22 +3,31 @@ import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { environment } from 'src/environments/environment';
 
 import { Observable } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { AppHttpErrorHandler } from 'src/app/core/utils/errorHandler';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 
 @Injectable({
   providedIn: 'root'
 })
-export class ValidaMailApiService  {
+export class ValidaMailApiService  extends AppHttpErrorHandler {
   private readonly BASEURL = environment.baseUrlApi;
-  constructor(private http: HttpClient) {
-   }
+  constructor(private http: HttpClient,
+              public router: Router,
+              public toast: ToastrService) {
+    super(router, toast);
+  }
 
   public validaMail(email: string, hash: string): Observable<boolean> {
     const params = {
         email,
         hash,
       };
-    return this.http.post<boolean>(this.BASEURL + 'user/emailValidation', params);
+    return this.http.post<boolean>(this.BASEURL + 'user/emailValidation', params).pipe(
+      catchError((err) => this.handleError(err))
+    );
   }
 
 }
