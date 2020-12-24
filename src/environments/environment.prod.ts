eec308a33c60d43b303e6a40de644cb3ac5fa5a5
OmarJimenez <omar.jimenez@innovateod.com>
2020-12-24 06:56:03 -0800

    update values

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrlApi: 'https://ghdistributedservicespreparationv1.azurewebsites.net/api/',
+  baseUrlApi: 'https://gastroherenciaapi.azurewebsites.net/api/',
 
 };
